f7db25e9ea7d1ef8a78598f88f02f48971348b3c
Hack Reactor Students <accounts@hackreactor.com>
2014-05-15 16:32:30 -0700

    Add mongo-config to replace Bookshelf (for sql) ORM with Mongoose (for mongoDB)

---
+++ app/mongo-config.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
+var urlSchema;
+var userSchema;
+
+var urlSchema = new mongoose.Schema({
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: {type: Number, default: 0}
+});
+
+urlSchema.pre('save', function(next) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
+});
+
+exports.Link = mongoose.model('Url', urlSchema);
+
+//========================================================
+
+var userSchema = new mongoose.Schema({
+  username: {type: String, required: true, index: {unique: true}},
+  password: {type: String, required: true}
+});
+
+userSchema.pre('save', function(next) {
+  var cipher = Promise.promisify(bcrypt.hash);
+  cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+    });
+  next();
+});
+
+userSchema.methods.comparePassword = function(attemptedPassword, cb){
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    cb(isMatch);
+  });
+};
+
+exports.User = mongoose.model('User', userSchema);
